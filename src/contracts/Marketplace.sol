c53eda0168108d637559fa3111d1fd2f159ddfab
Bruno Leone <leonecbruno@gmail.com>
2019-08-22 02:19:12 -0300

    Adicionando metodo para criação de produto

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,6 +2,15 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+    mapping (uint => Product) public products;
+    event ProductCriated(
+        uint id,
+        string nome,
+        uint price,
+        string owner,
+        bool purchased
+    );
 
     struct Product {
         uint id;
@@ -13,5 +22,18 @@ contract Marketplace {
 
     constructor () public {
         name = "";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        // Requere nome
+        require(byte(_name).lenght > 0);
+        // Requere um preço valido
+        require(_price > 0);
+        // Incrementa contagem de produtos
+        productCount++;
+        // Cria o produto
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // Emite evento de criado
+        emit ProductCriated(productCount, _name, _price, msg.sender, false);
     }
 }
